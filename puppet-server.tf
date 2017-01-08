65fc415e5932e96bb53141df2f3521ceeca66dc0
Shavit Netzer <shavitn@perfectomobile.com>
2017-01-08 18:23:52 +0200

    adding files

---
+++ puppet-server.tf
@@ -0,0 +1,28 @@
+variable "server_port" {
+description = "The port the server will use for HTTP requests" default = 8080
+}
+
+resource "aws_security_group" "instance" { name = "Shavit-Puppet-Server"
+ingress {
+from_port = "${var.server_port}"
+        to_port = "${var.server_port}"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+} }
+
+resource "aws_instance" "example" {
+ami = "ami-40d28157"
+instance_type = "t2.micro"
+vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+user_data = <<EOF
+                  #!/bin/bash
+                  echo "Hello, World" > index.html
+                  nohup busybox httpd -f -p "${var.server_port}" &
+EOF
+      tags {
+        Name = "Shavit-Puppet-Server"
+} }
+
+output "public_ip" {
+      value = "${aws_instance.example.public_ip}"
+}
